e5a4e36209c548389e483b6cde96173ffcce94f7
nwhite <c2lkn.t@gmail.com>
2022-09-30 12:03:39 -0500

    adds gh-pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,25 @@
 import adapter from '@sveltejs/adapter-static';
-
+import preprocess from 'svelte-preprocess';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter()
-	}
+ // Consult https://github.com/sveltejs/svelte-preprocess
+ // for more information about preprocessors
+ preprocess: preprocess(),
+
+ kit: {
+ // hydrate the <div id="svelte"> element in src/app.html
+  target: '#svelte',
+  adapter: adapter({
+    pages: 'build',
+    assets: 'build',
+    fallback: null
+  })
+ }
 };
+// const config = {
+// 	kit: {
+// 		adapter: adapter()
+// 	}
+// };
 
 export default config;
